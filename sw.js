c4e5d0b745f893e6b864ec963d6b42502859e888
Liugx <2218465124@qq.com>
2026-09-12 18:33:40 +0800

    Deploy SQL File Shelf

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'sql-file-shelf-v1';
+const ASSETS = ['./', './index.html', './app.js', './manifest.webmanifest'];
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
