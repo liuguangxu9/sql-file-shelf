73ed6cd4a9727725d064a04469acffb5349d9d4e
Liugx <2218465124@qq.com>
2026-09-13 00:59:48 +0800

    fix: pin file deletion action to column edge

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sql-file-shelf-v6';
+const CACHE = 'sql-file-shelf-v7';
 const ASSETS = ['./', './index.html', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
